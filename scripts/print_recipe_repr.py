e734be8cce2d0a94063b9deb28dbbc1061d9788b
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-05-18 10:47:45 -0700

    update script

--- scripts/print_recipe_repr.py
+++ scripts/print_recipe_repr.py
@@ -1,16 +1,1 @@
-import importlib
-import sys
-
-
-def get_instance(recipe_path, instance_name):
-    spec = importlib.util.spec_from_file_location("recipe", cache_path)
-    imported_module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(imported_module)
-    instance = getattr(imported_module, instance_name)
-    return instance
-
-
-if __name__ == "__main__":
-    recipe_path, instance_name = sys.argv[1:3]
-    instance = get_instance(recipe_path, instance_name)
-    print(instance)
+print(recipe_instance)
