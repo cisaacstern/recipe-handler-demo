3ab0bff92f2f1e58e8ca7150589555b334ec6117
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-05-18 16:40:49 -0700

    scriptify

--- scripts/print_recipe_repr.py
+++ scripts/print_recipe_repr.py
@@ -1,1 +1,10 @@
-print(recipe_instance)
+import sys
+
+if __name__ == "__main__":
+    import recipe
+
+    recipe_instance_name = sys.argv[1]
+
+    recipe_instance = getattr(recipe, recipe_instance_name)
+
+    print(recipe_instance)
